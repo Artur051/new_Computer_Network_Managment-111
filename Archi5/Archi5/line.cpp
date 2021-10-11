0bfa9a1342112b9d00cb3cd53bf6fd765d5c4e8a
Artur051 <mr.mazanovartur440@mail.ru>
2021-10-11 22:43:51 +0300

    задание 5
    Функция определяет, существуют ли прямые A1x+B1y+C1=0 и A2x+B2y+C2=0, если выражение d=A1B2-A2B1 не равно нулю. Прямые задаются структурой с тремя полями.

---
+++ Archi5/Archi5/line.cpp
@@ -0,0 +1,57 @@
+﻿#include "line.h"
+ostream& operator<<(ostream& os, const line& obj)
+{
+	return os << "(" << obj.a << "," << obj.b << "," << obj.c << ")";
+}
+ostream& operator<<(ostream& os, const exist& obj)
+{
+	return os << "(" << obj.first.a << "," << obj.first.b << "," << obj.first.c << ")" << " (" << obj.second.a << "," << obj.second.b << "," << obj.second.c << ")";
+}
+bool exist::double_compare(const double value_a, const double value_b)
+{
+	return abs(value_a - value_b) <= numeric_limits<double>::min();
+}
+ line exist::get_line1() const
+{
+	return first;
+}
+ line exist::get_line2() const
+ {
+	 return second;
+ }
+double exist::get_d() const
+{
+	return (first.a * second.b - second.a * first.b);
+}
+exist exist::noexep() const 
+{
+	return  exist(first.a, first.b, first.c, second.a, second.b, second.c);
+}
+exist exist::with_throw() throw()
+{
+	return  noexep();
+}
+exist exist::invalid_throw() throw(invalid_argument)
+{
+	if (double_compare(get_d(),0))
+		throw invalid_argument("Прямые не существуют");
+	return noexep();
+}
+exist exist::empty() throw(empty_exeption)
+{
+	if (double_compare(get_d(), 0))
+		throw empty_exeption();
+	return noexep();
+}
+exist exist::my_exp1() throw(my_exeption1)
+{
+	if (double_compare(get_d(), 0))
+		throw my_exeption1("Прямые не существуют");
+	return noexep();
+}
+exist exist::my_exp2() throw(my_exeption2)
+{
+	if (double_compare(get_d(), 0))
+		throw my_exeption2("выражение равно = ", get_d());
+	return noexep();
+}
---
+++ Archi5/Archi5/line.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+#include "myexeption.h"
+#include <cmath>
+#include <ostream>
+/*
+* brief Структура для хранения коэф прямых;
+*/
+struct line
+{
+	double a;
+	double b;
+	double c;
+	friend ostream& operator<<(ostream& os, const line& obj);
+};
+/*
+* \brief Класса для определиния существуют ли прямые
+*/
+class exist
+{
+private:
+	/*
+	* brief две линии
+	*/
+	line first;
+	line second;
+public:
+	/*
+	* \brief конструктор принимающий две прямых
+	*/
+	exist(const double a, const double b, const double c, const double a2, const double b2, const double c2)
+	{
+		this->first.a = a;
+		this->first.b = b;
+		this->first.c = c;
+		this->second.a = a2;
+		this->second.b = b2;
+		this->second.c = c2;
+	}
+	/*
+	* \breif деструктор по умолчанию
+	*/
+	~exist() = default;
+	/*
+	* \breif геттеры прямых и d
+	*/
+	line get_line1() const;
+	line get_line2() const;
+	double get_d() const;
+	/*
+	* \breif функция сравнивающая double значения
+	*/
+	bool double_compare(const double value_a, const double value_b);
+	/*
+	* \brief оператор вывода
+	*/
+	friend ostream& operator<<(ostream& os, const exist& obj);
+	/*
+	* \brief Функция без спецификации исключений
+	*/
+	exist noexep() const;
+	/*
+	* \brief Функция со спецификацией throw();
+	*/
+	exist with_throw() throw();
+	/*
+	* \brief Функция с конкретной спецификацией с подходящим стандартным исключением
+	*/
+	exist invalid_throw() throw(invalid_argument);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как пустой клас
+	*/
+	exist empty() throw(empty_exeption);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как независимый класс с полями-параметрами функции
+	*/
+	exist my_exp1() throw(my_exeption1);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как наследник от стандартного исключения с полями
+	*/
+	exist my_exp2() throw(my_exeption2);
+};
