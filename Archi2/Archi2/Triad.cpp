a222267012be98607f77e9eec20b29bde150c0e8
Artur051 <mr.mazanovartur440@mail.ru>
2021-10-11 22:34:29 +0300

    задание 3. вариант 16. наследование
    Создать базовый класс Triad (тройка чисел) с операциями сложения с числом, умножения на число, проверки на равенство. Создать производный класс vector3D, задаваемый тройкой координат. Должны быть реализованы: операция сложения векторов, скалярное произведение векторов.

---
+++ Archi2/Archi2/Triad.cpp
@@ -0,0 +1,29 @@
+﻿#include "Triad.h"
+Triad operator+(const Triad& lhs, const double rhs)
+{
+	const double num1 = lhs.num1 + rhs;
+	const double num2 = lhs.num2 + rhs;
+	const double num3 = lhs.num3 + rhs;
+	return Triad(num1,num2,num3);
+
+}
+Triad operator*(const Triad& lhs, const double rhs)
+{
+	const double num1 = lhs.num1 * rhs;
+	const double num2 = lhs.num2 * rhs;
+	const double num3 = lhs.num3 * rhs;
+	return Triad(num1, num2, num3);
+}
+bool operator==(const Triad& lhs, const Triad& rhs)
+{
+	return (abs(lhs.num1 - rhs.num1) <= numeric_limits<double>::min())
+		&& (abs(lhs.num2 - rhs.num2) <= numeric_limits<double>::min())
+		&& (abs(lhs.num3 - rhs.num3) <= numeric_limits<double>::min());
+}
+ostream& operator<<(ostream& os, const Triad& obj)
+{
+	return os << obj.num1 << "," << obj.num2 << "," << obj.num3;
+}
+double Triad::get_num1() const { return num1; }
+double Triad::get_num2() const { return num2; }
+double Triad::get_num3() const { return num3; }
---
+++ Archi2/Archi2/Vector3D.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Triad.h"
+/*
+* \breif класс Vector3d (наследник)
+*/
+class Vector3D : public Triad
+{
+private:
+
+public:
+	/*
+	* \breif конструктор с параметрами
+	*/
+	Vector3D(const double x, const double y, const double z) : Triad(x,y,z){}
+	/*
+	* \breif деструктор по умолчанию
+	*/
+	~Vector3D() = default;
+	/*
+	* \breif опретор сложения векторов
+	*/
+	friend Vector3D operator+(const Vector3D& A, const Vector3D& B);
+	/*
+	* \breif оператор для нахождения скалярного произведения
+	*/
+	friend double operator*(const Vector3D& A, const Vector3D& B);
+};
