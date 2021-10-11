474e6919f8133b726770377c63f1849c896b8b8d
Artur051 <mr.mazanovartur440@mail.ru>
2021-10-11 22:28:44 +0300

    задание 1-2. написать API класс.
    Создать класс Goods (товар). В классе должны быть представлены поля: наименование товара, дата оформления, цена товара, количество единиц товара, номер накладной, по которой товар поступил на склад. Реализовать методы изменения цены товара, изменение количества товара (увеличения и уменьшения), вычисление стоимости товара. Метод toString() должен выдавать в виде строки стоимость товара.

---
+++ archi1/archi1/Goods.cpp
@@ -0,0 +1,74 @@
+﻿#include "Goods.h"
+ostream& operator<<(std::ostream& os, const price& obj)
+{
+	return os << obj.rub << "," << obj.cop;
+}
+ostream& operator<<(std::ostream& os, const date& obj)
+{
+	return os << obj.days << "." << obj.month << "." << obj.years;
+}
+ostream& operator<<(std::ostream& os, const Goods& obj)
+{
+	return os << "Наименование товара: " << obj.name << ";" <<"\n" <<"Дата оформления (" << obj.data << ");"<< "\n" << "Цена товара = "
+		<< obj.cost << ";" << "\n" << "Количество товара: " << obj.count << " единиц;" << "\n" << "Номер накладной: " << obj.id << ";" << "\n" << "Сумма стоимости товара = " << obj.sum_price();
+}
+Goods::Goods(const string name, const unsigned int rub, const unsigned int cop, const unsigned int count, const unsigned int day, const unsigned int month, const unsigned int year)
+{
+	this->name = name;
+	change_price(rub,cop);
+	change_count(count);
+	change_date(day, month, year);
+	number++;
+	this->id = number;
+}
+void Goods::change_price(const unsigned int rub,const unsigned int cop)
+{
+	this->cost.rub = rub;
+	this->cost.cop = cop;
+}
+void Goods::change_count(const unsigned int count)
+{
+	this->count = count;
+}
+void Goods::change_date(const unsigned int day, const unsigned int month, const unsigned int year)
+{
+	if (real_date(day, month, year)) {
+		this->data.days = day;
+		this->data.month = month;
+		this->data.years = year;
+	}
+}
+price Goods::sum_price() const 
+{
+	price temp;
+	temp.rub = cost.rub * count;
+	temp.cop = cost.cop * count;
+	if(temp.cop>=100)
+	{
+		while (temp.cop>100)
+		{
+			temp.cop = temp.cop -100;
+			temp.rub = temp.rub + 1;
+		}
+	}
+	return temp;
+}
+bool Goods::real_date(const unsigned int day,const unsigned int month,const unsigned int year) const
+{
+	if (day <= 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+		return true;
+	}
+	if (day <= 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+		return true;
+	}
+	if (day <= 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+		return true;
+	}
+	if (day <= 28 && (year % 400 != 0 || (year % 4 != 0 && year % 100 == 0))) {
+		return true;
+	}
+	throw ("Дата не настоящая!");
+	return false;
+}
+
+int Goods::number = 0;
---
+++ archi1/archi1/Goods.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+#include <iostream>
+#include <ostream>
+using namespace std;
+/*
+* \brief структура цены
+*/
+struct price
+{
+	unsigned int rub;
+	unsigned int cop;
+	friend	ostream& operator<<(std::ostream& os, const price& obj);
+};
+/*
+* \breif структура даты
+*/
+struct date
+{
+	unsigned int days;
+	unsigned int month;
+	unsigned int years;
+	friend	ostream& operator<<(std::ostream& os, const date& obj);
+};
+/*
+* \breif класс товар
+*/
+class Goods
+{
+private:
+	/*
+	* \brief переменные содержацие информацию о товаре
+	*/
+	string name;
+	price cost;
+	unsigned int count;
+	date data;
+	int id;
+public:
+	/*
+	* \breif статическая переменная для генерации номера накладной
+	*/
+	static int number;
+	/*
+	* \breif коструктор с параметрами
+	*/
+	Goods(const string name, const unsigned int rub, unsigned const int cop, const unsigned int count, const unsigned int day, const unsigned int month, const unsigned int year);
+	/*
+	* \breif конструктор копирования
+	*/
+	Goods(Goods& other) = default;
+	/*
+	* \breif диструктор по умолчанию
+	*/
+	~Goods() = default;
+	/*
+	* \breif дружественная функция для вывода общей информации
+	*/
+	friend	ostream& operator<<(std::ostream& os, const Goods& obj);
+	/*
+	* \breif сеттер цены
+	*/
+	void change_price(const unsigned int rub, const unsigned int cop);
+	/*
+	* \breif cеттер количества товара
+	*/
+	void change_count(const unsigned int count);
+	/*
+	* \brief сеттер даты
+	*/
+	void change_date(const unsigned int day, const unsigned int month, const unsigned int year);
+	/*
+	* \breif суммарная стоимость товара
+	*/
+	price sum_price() const;
+	/*
+	* \breif проверка даты на её правдоподобность
+	*/
+	bool real_date(const unsigned int day, const unsigned int month,const unsigned int year) const;
+
+};
