d9097b164b49eb4a8efbf1b27c6a44a4ae43f417
Artur051 <mr.mazanovartur440@mail.ru>
2021-10-11 22:20:39 +0300

    задание 1-2. написать API класса.
    Создать класс Goods (товар). В классе должны быть представлены поля: наименование товара, дата оформления, цена товара, количество единиц товара, номер накладной, по которой товар поступил на склад. Реализовать методы изменения цены товара, изменение количества товара (увеличения и уменьшения), вычисление стоимости товара. Метод toString() должен выдавать в виде строки стоимость товара.

---
+++ archi1/archi1/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "Goods.h"
+void main() 
+{
+	setlocale(LC_ALL, "ru");
+	Goods first("имя", 10, 60, 2, 28, 2, 2001);
+	cout << first.sum_price()<< "\n";
+	first.change_count(5);
+	first.change_price(20, 50);
+	cout << first;
+}
